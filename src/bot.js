5dff93ad834ea7bd0745e5cf6d6e3cd94b58e1f8
RedStewart <reedostewart@gmail.com>
2021-09-04 13:45:29 +1200

    rename entry file

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,8 @@ const MinecraftAPI = require('./MinecraftAPI');
 const Logger = require('../tools/Logger');
 
 const main = async () => {
-  const mcapi = new MinecraftAPI();
-  await mcapi.getData();
+  // const mcapi = new MinecraftAPI();
+  // await mcapi.getData();
 };
 
 main();
