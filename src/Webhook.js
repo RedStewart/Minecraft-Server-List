124c4763cd4ce631756b31d9180bdc91ae550070
redstewart <reedostewart@gmail.com>
2021-09-06 02:19:11 +1200

    Adjusting webhook sending with an edit message option

--- src/Webhook.js
+++ src/Webhook.js
@@ -10,13 +10,13 @@ class Webhook {
       'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/71c6fceb-a2b9-4ccb-9af4-387c330f23e5/d4j5vy7-b3e8d628-ced2-4c4a-89c4-bb1e3fa953dd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzcxYzZmY2ViLWEyYjktNGNjYi05YWY0LTM4N2MzMzBmMjNlNVwvZDRqNXZ5Ny1iM2U4ZDYyOC1jZWQyLTRjNGEtODljNC1iYjFlM2ZhOTUzZGQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.5YmIy5ClmnCQOeF5HxVCXxouwkLvgKvFPp4EueJCyqw';
   }
 
-  async sendWebhook(type = 'new') {
+  async sendWebhook(type = 'update') {
     switch (type) {
       case 'new':
         return await this.channel.send(this.getWebhookFormat());
 
       case 'update':
-        break;
+        return await this.embedMessage.edit(this.getWebhookFormat());
 
       default:
         break;
@@ -33,7 +33,9 @@ class Webhook {
       embeds: [
         new MessageEmbed()
           .setTitle(`**${this.mcServcer.serverIp}**`)
-          .setColor('#0099ff')
+          .setColor(
+            this.mcServcer.playerList.length === 0 ? '#ee5253' : '#1dd1a1'
+          )
           .setAuthor('Player List Updated!', this.iconUrl)
           .setDescription(this.mcServcer.motd)
           .setThumbnail(this.mcServcer.icon)
