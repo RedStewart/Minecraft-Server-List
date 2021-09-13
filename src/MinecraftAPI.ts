a4ca084495e9616e703fa0f83afed8f6b1d6411e
RedStewart <reedostewart@gmail.com>
2021-09-14 11:35:12 +1200

    Convert MinecraftAPI class to TS

--- src/MinecraftAPI.ts
+++ src/MinecraftAPI.ts
@@ -1,16 +1,18 @@
 const mcServerUtil = require('minecraft-server-util');
 
 const Config = require('../config/Config.json');
-const Logger = require('../tools/Logger');
+const Logger = require('../tools/CustomLogger');
 
 class MinecraftAPI {
+  private serverIp: string;
+
   constructor() {
     this.serverIp = Config.serverIp;
   }
 
-  async getData() {
+  async getData(): Promise<object | undefined> {
     try {
-      const res = await mcServerUtil.status(this.serverIp);
+      const res: object = await mcServerUtil.status(this.serverIp);
       return res;
     } catch (e) {
       Logger.error(e);
